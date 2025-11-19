5b428f26cddecf36e9544945276baf00f2e04f2b
sachidananda sahoo <sachidananda.sahoo@BRILLIO.COM>
2025-11-19 22:00:54 +0530

    poc code

---
+++ src/test/java/org/rahulshettyacademy/eCommerce_tc_cloud.java
@@ -0,0 +1,52 @@
+package org.rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.rahulshettyacademy.TestUtils.AndroidBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
+
+public class eCommerce_tc_cloud extends AndroidBaseTest{
+
+	
+
+	
+	@Test
+
+	public void FillForm_ErrorValidation() throws InterruptedException
+	{
+		
+	//	driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rahul Shetty");
+		driver.hideKeyboard();
+		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+		driver.findElement(By.id("android:id/text1")).click();
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+		AssertJUnit.assertEquals(toastMessage,"Please enter your name");
+		
+		
+			
+	}
+	
+
+	
+}
